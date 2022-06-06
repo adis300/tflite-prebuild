6d7c56f9cf65e353b9d619507eaab5d5030a81f9
Disi A <adis300>
2022-06-07 06:49:29 +0800

    Update build win.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # tflite-prebuild
 Prebuild tflite for many platforms.
+
+# Makesure bazel >= 5.1 is installed
+choco install bazel --version=5.1.0
--- build-win.bat
+++ build-win.bat
@@ -2,12 +2,13 @@ set SCRIPT_DIR=%cd%
 set TF_SRC_DIR=%SCRIPT_DIR%\tensorflow
 set BUILD_DIR=%SCRIPT_DIR%\tflite_build
 
-mkdir %BUILD_DIR%
-cd %BUILD_DIR%
+cd %TF_SRC_DIR%
+python configure.py
+bazel build //tensorflow/lite/c:tensorflowlite_c.dll -c opt
 
-call cmake %TF_SRC_DIR%/tensorflow/lite -G "MinGW Makefiles"
+RMDIR %BUILD_DIR%
+MKDIR %BUILD_DIR%\win
 
-::build\shared 
-call cmake --build . -j --config Release
-
+XCOPY 
 cd %SCRIPT_DIR%
+COPY %SCRIPT_DIR%\bazel-out\x64_windows-opt\bin\tensorflow\lite\c\*.dll %BUILD_DIR%\win /Y
