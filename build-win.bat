26913ce3dc5d49cc27963bfdacdd1580c802788c
Disi A <adis300>
2022-06-07 15:20:18 +0800

    Bug fix.

--- build-win.bat
+++ build-win.bat
@@ -8,7 +8,12 @@ bazel build //tensorflow/lite/c:tensorflowlite_c.dll -c opt
 
 RMDIR %BUILD_DIR%
 MKDIR %BUILD_DIR%\win
+MKDIR %BUILD_DIR%\include
 
 XCOPY 
 cd %SCRIPT_DIR%
 COPY %SCRIPT_DIR%\bazel-out\x64_windows-opt\bin\tensorflow\lite\c\*.dll %BUILD_DIR%\win /Y
+
+COPY %TF_SRC_DIR%\tensorflow\lite\c\c_api_types.h %BUILD_DIR%\include /Y
+COPY %TF_SRC_DIR%\tensorflow\lite\c\c_api.h %BUILD_DIR%\include /Y
+
