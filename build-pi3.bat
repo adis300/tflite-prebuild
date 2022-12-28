db1d48af9f91666b9db82381ff91a726fbcc5e5c
adis@brainco.tech <adis@brainco.tech>
2022-12-28 18:15:47 +0800

    Bug fixes.

--- build-pi3.bat
+++ build-pi3.bat
@@ -7,11 +7,11 @@ python configure.py
 bazel build --config=elinux_armhf -c opt //tensorflow/lite/c:libtensorflowlite_c.so
 
 RMDIR %BUILD_DIR%
-MKDIR %BUILD_DIR%\pi
+MKDIR %BUILD_DIR%\pi3
 MKDIR %BUILD_DIR%\include
 
 cd %SCRIPT_DIR%
-::COPY %SCRIPT_DIR%\bazel-out\x64_windows-opt\bin\tensorflow\lite\c\*.dll %BUILD_DIR%\win /Y
+COPY %SCRIPT_DIR%\bazel-out\armhf-opt\bin\tensorflow\lite\c\*.dll %BUILD_DIR%\pi3 /Y
 
 COPY %TF_SRC_DIR%\tensorflow\lite\c\c_api_types.h %BUILD_DIR%\include /Y
 COPY %TF_SRC_DIR%\tensorflow\lite\c\c_api.h %BUILD_DIR%\include /Y
--- build-pi4.sh
+++ build-pi4.sh
@@ -10,16 +10,15 @@ function echo_y() { echo -e "\033[1;33m$@\033[0m" ; }   # yellow
 function echo_r() { echo -e "\033[0;31m$@\033[0m" ; }   # red
 
 rm -rf $BUILD_DIR
-ARCH_NAME='armhf'
-# bazel build --config opt --cpu=darwin_arm64 --host_cpu=darwin_arm64 //tensorflow/tools/lib_package:libtensorflow
+ARCH_NAME='aarch64'
 cd $TF_SRC_DIR
 python3 configure.py
 
 bazel build --config=elinux_$ARCH_NAME -c opt //tensorflow/lite/c:libtensorflowlite_c.so
 
-mkdir -p $BUILD_DIR/pi3/$ARCH_NAME
+mkdir -p $BUILD_DIR/pi4/$ARCH_NAME
 
-cp $TF_SRC_DIR/bazel-out/$ARCH_NAME-opt/bin/tensorflow/lite/c/libtensorflowlite_c.so  $BUILD_DIR/pi3/$ARCH_NAME/libtensorflowlite_c.so
+cp $TF_SRC_DIR/bazel-out/$ARCH_NAME-opt/bin/tensorflow/lite/c/libtensorflowlite_c.so  $BUILD_DIR/pi4/$ARCH_NAME/libtensorflowlite_c.so
 
 cd $SCRIPT_DIR
 mkdir -p $BUILD_DIR/include
