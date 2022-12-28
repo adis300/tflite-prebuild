919ea7c96cf93299393ba1747546b7b5cd4de20c
Disi A <adis300>
2022-12-28 15:12:47 +0800

    Add raspberry pi3 command.

---
+++ build-pi3.bat
@@ -0,0 +1,17 @@
+set SCRIPT_DIR=%cd%
+set TF_SRC_DIR=%SCRIPT_DIR%\tensorflow
+set BUILD_DIR=%SCRIPT_DIR%\tflite_build
+
+cd %TF_SRC_DIR%
+python configure.py
+bazel build --config=elinux_armhf -c opt //tensorflow/lite/c:libtensorflowlite_c.so
+
+RMDIR %BUILD_DIR%
+MKDIR %BUILD_DIR%\pi
+MKDIR %BUILD_DIR%\include
+
+cd %SCRIPT_DIR%
+::COPY %SCRIPT_DIR%\bazel-out\x64_windows-opt\bin\tensorflow\lite\c\*.dll %BUILD_DIR%\win /Y
+
+COPY %TF_SRC_DIR%\tensorflow\lite\c\c_api_types.h %BUILD_DIR%\include /Y
+COPY %TF_SRC_DIR%\tensorflow\lite\c\c_api.h %BUILD_DIR%\include /Y
